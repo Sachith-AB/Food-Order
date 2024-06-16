d67df7ff7036f3192258656bbeeef9e516f1f963
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-06-16 11:34:55 +0530

    create footer section UI part

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>food order website - Home page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu section start -->
+     <div class="menu">
+        <div class="wrapper">
+            Menu Goes Here
+        </div>
+     </div>
+     <!-- Menu section end -->
+
+     <!-- Main section start -->
+      <div>
+        <div class="wrapper">
+            Main Content Goes Here
+        </div>
+      </div>
+     <!-- Main section end -->
+
+     <!-- Footer section start -->
+      <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022 All rights reserved, Some Resturant. Developed By - <a href="https://github.com/Sachith45">Sachith Abeywardhana</a></p>
+        </div>
+      </div>
+      <!-- Fotter section end -->
+</body>
+</html>
