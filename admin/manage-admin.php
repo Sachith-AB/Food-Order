ac7b737d30ddf74388ff656353bd953036b4963d
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-06-17 10:46:43 +0530

    create manage admin page UI

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,8 +5,52 @@
      <!-- Main section start -->
       <div class="main-content">
         <div class="wrapper">
-            <h2>Manage Admin</h2>
-            
+            <h2>Manage Admin</h2><br>
+
+            <a href="#" class=""> Add Admin</a><br><br><br><br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>sachith abeywardhana</td>
+                    <td>sachith2002</td>
+                    <td>
+                        <div class="col">
+                            <a class="edit-btn">Edit</a>
+                            <a class="delete-btn">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>sachith abeywardhana</td>
+                    <td>sachith2002</td>
+                    <td>
+                        <div class="col">
+                            <a class="edit-btn">Edit</a>
+                            <a class="delete-btn">Delete</a>
+                       </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>sachith abeywardhana</td>
+                    <td>sachith2002</td>
+                    <td class="">
+                        <div class="col">
+                            <a class="edit-btn">Edit</a>
+                            <a class="delete-btn">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+                
+            </table>
         </div>
       </div>
      <!-- Main section end -->
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -3,6 +3,51 @@
 <div class="main-content">
     <div class="wrapper">
         <h2>Manage Category</h2>
+
+        <a href="#" class=""> Add Admin</a><br><br><br><br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>sachith abeywardhana</td>
+                    <td>sachith2002</td>
+                    <td>
+                        <div class="col">
+                            <a class="edit-btn">Edit</a>
+                            <a class="delete-btn">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>sachith abeywardhana</td>
+                    <td>sachith2002</td>
+                    <td>
+                        <div class="col">
+                            <a class="edit-btn">Edit</a>
+                            <a class="delete-btn">Delete</a>
+                       </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>sachith abeywardhana</td>
+                    <td>sachith2002</td>
+                    <td class="">
+                        <div class="col">
+                            <a class="edit-btn">Edit</a>
+                            <a class="delete-btn">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+                
+            </table>
     </div>
 </div>
 <?php include('partials/footer.php'); ?>
