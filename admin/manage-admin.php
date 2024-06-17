7ed27de8726876322502e8cc8e3a62123ca2fc1f
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-06-17 12:48:01 +0530

    complete manage page UI

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -23,39 +23,43 @@
                     <th>Username</th>
                     <th>Actions</th>
                 </tr>
-                <tr>
-                    <td>1.</td>
-                    <td>sachith abeywardhana</td>
-                    <td>sachith2002</td>
-                    <td>
-                        <div class="col">
-                            <a class="edit-btn">Edit</a>
-                            <a class="delete-btn">Delete</a>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2.</td>
-                    <td>sachith abeywardhana</td>
-                    <td>sachith2002</td>
-                    <td>
-                        <div class="col">
-                            <a class="edit-btn">Edit</a>
-                            <a class="delete-btn">Delete</a>
-                       </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3.</td>
-                    <td>sachith abeywardhana</td>
-                    <td>sachith2002</td>
-                    <td class="">
-                        <div class="col">
-                            <a class="edit-btn">Edit</a>
-                            <a class="delete-btn">Delete</a>
-                        </div>
-                    </td>
-                </tr>
+
+                <?php 
+                $sql = 'SELECT * FROM tbl_admin';
+                $res = mysqli_query($conn, $sql);
+                if($res == true){
+                    $count = mysqli_num_rows($res);
+
+                    $sn = 1;
+
+                    if($count > 0){
+                        while($rows = mysqli_fetch_assoc($res)){
+                            $id = $rows['id'];
+                            $full_name = $rows['full_name'];
+                            $username = $rows['username'];
+                            
+                            ?>
+                                <tr>
+                                    <td><?php echo $sn++ ?></td>
+                                    <td><?php echo $full_name ?></td>
+                                    <td><?php echo $username ?></td>
+                                    <td>
+                                        <div class="col">
+                                            <a class="edit-btn">Edit</a>
+                                            <a class="delete-btn">Delete</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    }else{
+                        echo'Not admin yet';
+                    }
+                }
+                ?>
+                
+                
+               
                 
             </table>
         </div>
